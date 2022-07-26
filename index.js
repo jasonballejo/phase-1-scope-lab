a2c3825406e4c1da0165caeb3e68bc1b80cd34f2
Jason Ballejo <ballejo19@gmail.com>
2022-07-26 13:41:08 -0400

    commit tree 0e04955fc4ed7acc21a2271c3fe4c3b4531504ed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = `not ${customerName.toLowerCase()}`
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 2
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 3
+    console.log(changeLeastFavoriteCustomer)
+}
